2ae2ac4972e0aaa72624bf5f0b564b29aee3e313
Mich <125368412+michellycruz@users.noreply.github.com>
2024-01-20 17:28:07 -0300

    Update script.js

--- 1_conversor_de_unidades/script.js
+++ 1_conversor_de_unidades/script.js
@@ -4,3 +4,20 @@ const toElement = document.querySelector('#to')
 const outputElement = document.querySelector('#output')
 const convertButton = document.querySelector('#convert-btn')
 const messageElement = document.querySelector('#message')
+
+
+
+const convert = () => {
+    const fromValue = fromElement.value
+    const toValue = toElement.value
+
+    if(fromValue === toValue){
+        outputElement.value = inputElement.value;
+        messageElement.textContent = "";
+        return
+    }
+
+    console.log(fromValue, toValue);
+}
+
+convertButton.addEventListener("click", convert)
