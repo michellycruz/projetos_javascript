66b7aac927014e2d77d34968187ab5223195ffae
Mich <125368412+michellycruz@users.noreply.github.com>
2024-01-20 19:43:55 -0300

    arrumei a lógica para a exibição da mensagem

--- 1_conversor_de_unidades/script.js
+++ 1_conversor_de_unidades/script.js
@@ -7,17 +7,56 @@ const messageElement = document.querySelector('#message')
 
 
 
-const convert = () => {
+function convert() {
     const fromValue = fromElement.value
     const toValue = toElement.value
 
     if(fromValue === toValue){
         outputElement.value = inputElement.value;
         messageElement.textContent = "";
-        return
+        return;
     }
 
-    console.log(fromValue, toValue);
+    let meters;
+    switch(fromValue){
+        case 'm':
+            meters = inputElement.value;
+        break
+        case 'km':
+            meters = inputElement.value * 1000;
+        break
+        case 'cm':
+            meters = inputElement.value / 100;
+        break
+        case 'mm':
+            meters = inputElement.value / 1000;
+        break
+    }
+
+    let result;
+    switch(toValue){
+        case 'm':
+            result = meters;
+        break
+        case 'km':
+            result = meters / 1000;
+        break
+        case 'cm':
+            result = meters * 100;
+        break
+        case 'mm':
+            result = meters * 1000;
+        break
+    }
+
+    outputElement.value = result
+
+    const fromLabel = fromElement.options[fromElement.selectedIndex].text
+    const toLabel = toElement.options[toElement.selectedIndex].text
+
+    const message = `${inputElement.value} ${fromLabel} equivalem a ${result} ${toLabel}`
+    messageElement.textContent = message
+    return;
 }
 
 convertButton.addEventListener("click", convert)
