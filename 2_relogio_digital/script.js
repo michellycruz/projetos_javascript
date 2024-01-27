593584f784ac08ca2c33ea5e3a3ad8b1c31afa18
Mich <125368412+michellycruz@users.noreply.github.com>
2024-01-27 17:36:14 -0300

    js lógica do relogio

---
+++ 2_relogio_digital/script.js
@@ -0,0 +1,18 @@
+function updateClock(){
+    const hoursElement = document.querySelector(".hours");
+    const minutesElement = document.querySelector(".minutes");
+    const secondsElement = document.querySelector(".seconds");
+
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, "0");
+    const minutes = now.getMinutes().toString().padStart(2, "0");
+    const seconds = now.getSeconds().toString().padStart(2, "0");
+
+    hoursElement.textContent = hours;
+    minutesElement.textContent = minutes;
+    secondsElement.textContent = seconds;
+}
+
+
+
+updateClock();
