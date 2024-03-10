5eaa0bd7413adaa820cfe1c9f5088d522e267cbf
Mich <125368412+michellycruz@users.noreply.github.com>
2024-03-10 15:30:58 -0300

    Update script.js

---
+++ 6_formulario_com_validacao/script.js
@@ -0,0 +1,5 @@
+const form = document.querySelector("form");
+const nome = document.querySelector("#nome");
+const email = document.querySelector("#email");
+const assunto = document.querySelector("#assunto");
+const mensagem = document.querySelector("#mensagem");
