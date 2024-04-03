de7a7cbc99419854d8c8a8b4a49e9d8ee7558713
Mich <125368412+michellycruz@users.noreply.github.com>
2024-04-02 22:50:00 -0300

    gerador de cpf logica

---
+++ 8_gerador_de_cpf/script.js
@@ -0,0 +1,29 @@
+const cpfEl = document.querySelector("#cpf");
+const gerarCpfBtn = document.querySelector("#gerar-cpf")
+const copiarCpfBtn = document.querySelector("#copiar-cpf")
+
+function gerarCPF(){
+    let n = Math.floor(Math.random() * 999999999) + 1;
+    let nStr = n.toString().padStart(9, "0");
+    let dv1 = calcularDV(nStr, 10);
+    let dv2 = calcularDV(nStr + dv1, 11);
+
+    cpfEl.innerText = formatarCPF(nStr + dv1 + dv2);
+}
+
+function calcularDV(numero, peso){
+    let total = 0;
+    for(let i = 0; i < numero.length; i++){
+        total += parseInt(numero.charAt(i)) * peso--;
+    }
+
+    let resto = total % 11;
+    return resto < 2 ? 0 : 11 - resto;
+}
+
+function formatarCPF(cpf) {
+    const regex = /^(\d{3})(\d{3})(\d{3})(\d{2})$/;
+    return cpf.replace(regex, "$1.$2.$3-$4")
+}
+
+gerarCpfBtn.addEventListener("click", gerarCPF)
