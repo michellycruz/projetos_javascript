2206f66410bb89c223b2b5b70808a326a8521d85
Mich <125368412+michellycruz@users.noreply.github.com>
2024-06-09 23:21:30 -0300

    cards

--- 10_jogo_da_memoria/script.js
+++ 10_jogo_da_memoria/script.js
@@ -19,6 +19,36 @@ function shuffleCards(cards){
 async function createCards(){
     const imagePairs = await generateImagePairs()
     shuffleCards(cards);
+    const cardList = document.querySelector(".container")
+    for(let i = 0; i < cards.length; i++){
+
+        const card = document.createElement("div")
+        const cardBack = document.createElement("div")
+        const cardFront = document.createElement("div")
+
+        card.classList.add("card")
+        cardBack.classList.add("back")
+        cardFront.classList.add("front")
+
+        cardBack.style.backgroundImage = `url('img/card-back.png')`;
+
+        const cardNumber = cards[i]
+        const cardImage = imagePairs[cardNumber].pop();
+
+        cardFront.style.backgroundImage = `url(${cardImage})`
+
+        card.setAttribute("data-card", cardNumber)
+        card.appendChild(cardBack)
+        card.appendChild(cardFront)
+        card.addEventListener("click", flipCard)
+        cardList.appendChild(card)
+    }
+}
+
+
+
+function flipCard(){
+    
 }
 
 createCards()
