bf1dd84ba808e37566917c7f0d2ff638320593ab
Mich <michxcrz@gmail.com>
2024-06-28 13:18:12 -0300

    js gerador de senha

---
+++ 21_gerador_de_senhas/js/scripts.js
@@ -0,0 +1,57 @@
+// seleção de elementos
+
+const generatePasswordButton = document.querySelector("#generate-password")
+const generatePasswordElement = document.querySelector("#generate-password")
+
+// funções
+const getLetterLowerCase = () => {
+    return console.log(String.fromCharCode(Math.floor(Math.random() * 26) + 97))
+}
+
+const getLetterUpperCase = () => {
+    return console.log(String.fromCharCode(Math.floor(Math.random() * 26) + 65))
+}
+
+const getNumber = () => {
+    return Math.floor(Math.random() * 10).toString();
+}
+
+const getSymbol = () => {
+    const symbols = "()<>{}[]/?!@#$%&*-_'+.,=";
+    return symbols[Math.floor(Math.random() * symbols.length)]
+}
+
+const generatePassword = (getLetterLowerCase, getLetterUpperCase, getNumber, getSymbol) => {
+
+    let password = ""
+
+    const passwordLength = 10
+
+    const generators = [
+        getLetterLowerCase,
+        getLetterUpperCase,
+        getNumber,
+        getSymbol
+    ]
+
+    for(i = 0; i < passwordLength; i = i + 4) {
+        generators.forEach(() => {
+            const randomValue = generators[Math.floor(Math.random() * generators.length)]();
+            password += randomValue
+        })
+    }
+    console.log(password)
+}
+
+
+getLetterLowerCase()
+
+// eventos
+generatePasswordButton.addEventListener("click", () => {
+    generatePassword(
+        getLetterLowerCase,
+        getLetterUpperCase,
+        getNumber,
+        getSymbol
+    )
+})
